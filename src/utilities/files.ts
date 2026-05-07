3bbbccb23c4e68ced77c4477c5f4df9de0ef2014
mineners <76274288+minenersGaming@users.noreply.github.com>
2026-05-07 23:24:40 +0700

    temporarily referred to googleapis for images

--- src/utilities/files.ts
+++ src/utilities/files.ts
@@ -1,17 +1,22 @@
-export const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-});
+export const toBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = (error) => reject(error)
+  })
 
 export const convertToStaticFileUriC = (isStatic, old: string) => {
-    if (!isStatic) {
-        return old
-    }
-    return old.replace("storage.googleapis.com", "static-clubreg.tucm.cc").replace("/assets", "https://static-clubreg.tucm.cc/assets")
+  if (!isStatic) {
+    return old
+  }
+  //static-clubreg.tucm.cc doesn't seems to work at the moment
+  //return old.replace("storage.googleapis.com", "static-clubreg.tucm.cc").replace("/assets", "https://static-clubreg.tucm.cc/assets")
+  return old
+    .replace("storage.googleapis.com", "storage.googleapis.com")
+    .replace("/assets", "https://storage.googleapis.com/assets")
 }
 
 export const convertToStaticFileUri = (old: string) => {
-    return convertToStaticFileUriC(true, old)
+  return convertToStaticFileUriC(true, old)
 }
